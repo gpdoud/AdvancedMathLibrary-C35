f46924ef508f2c3dee400fc1a02a3ae65f6397e8
Greg Doud <gdoud@gmail.com>
2022-02-10 11:55:01 -0500

    Add project files.

---
+++ AdvancedMathLibrary/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AdvancedMathLibrary {
+
+    public static class Math {
+
+        public const int Zero = 0;
+
+        public static int AbsoluteValue(int x) {
+            return (x < 0) ? -x : x;
+        }
+        public static int Add(int x, int y) {
+            return x + y;
+        }
+        public static int Subtract(int x, int y) {
+            return x - y;
+        }
+        public static int Multiply(int x, int y) {
+            return x * y;
+        }
+        public static int Divide(int x, int y) {
+            if(y == 0) {
+                return 0;
+            }
+            return x / y;
+        }
+    }
+}
---
+++ TestAdvancedMathLibrary/TestIntMathLib.cs
@@ -0,0 +1,34 @@
+
+using AdvancedMathLibrary;
+using Xunit;
+
+namespace TestAdvancedMathLibrary {
+    public class TestIntMathLib {
+
+        [Fact]
+        public void TestDivideByZero() {
+            System.Action ZeroDenominator = () => Math.Divide(1, 0);
+            Assert.Throws<System.DivideByZeroException>(ZeroDenominator);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(0, 5, -5)]
+        [InlineData(-10, 15, -25)]
+        public void TestAdd(int expected, int a, int b) {
+            Assert.Equal(expected, Math.Add(a, b));
+        }
+
+        [Theory]
+        [InlineData(-3, 3)]
+        [InlineData(3, 3)]
+        [InlineData(0, 0)]
+        public void TestAbsoluteValue(int input, int expected) {
+            Assert.Equal(expected, Math.AbsoluteValue(input));
+        }
+        [Fact]
+        public void TestZeroConstant() {
+            Assert.Equal(0, Math.Zero);
+        }
+    }
+}
